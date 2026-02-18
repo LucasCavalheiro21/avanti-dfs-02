cf4146820a6cb82e27943dd09da451b0e3e799ff
Lucas Cavalheiro <l.cavalheirolc@gmail.com>
2026-02-18 01:35:04 -0300

    update

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
-const router = express.Router();
-
 import pessoasController from "./controllers/pessoasController.js";
+
+const routes = express.Routes();
 
 router.post("/pessoas", pessoasController.create);
 router.get("/pessoas", pessoasController.list);
@@ -9,4 +9,4 @@ router.get("/pessoas/:id", pessoasController.getById);
 router.put("/pessoas/:id", pessoasController.update);
 router.delete("/pessoas/:id", pessoasController.remove);
 
-export default router;
+export default routes;
